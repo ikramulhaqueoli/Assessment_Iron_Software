c66c41d0021ce0d2b584dfd0ce758b8195d9fe57
Ikramul Haque Oli <ikramul.haque@selisegroup.com>
2024-05-22 23:38:10 +0600

    Add comments to cases

--- Program.cs
+++ Program.cs
@@ -9,15 +9,16 @@
         public static void Main(params string[] args)
         {
             var results = new List<string> {
-                Phone.OldPhonePad("33#"),
-                Phone.OldPhonePad("227*#"),
-                Phone.OldPhonePad("4433555 555666#"),
-                Phone.OldPhonePad("8 88777444666*664#"),
-                Phone.OldPhonePad("22222#"),
-                Phone.OldPhonePad("22***2225#"),
-                Phone.OldPhonePad("***2225#"),
-                Phone.OldPhonePad("**#"),
-                Phone.OldPhonePad("#")
+                Phone.OldPhonePad("33#"),                       // Sample Case 1: E
+                Phone.OldPhonePad("227*#"),                     // Sample Case 2: B
+                Phone.OldPhonePad("4433555 555666#"),           // Sample Case 3: HELLO
+                Phone.OldPhonePad("8 88777444666*664#"),        // Sample Case 4: TURING
+
+                Phone.OldPhonePad("22222#"),                    // Edge Case 1: B (Circulate when pressed > 3 times)
+                Phone.OldPhonePad("22***2225#"),                // Edge Case 2: CJ (Do not remove next letters when extra backspace pressed before)
+                Phone.OldPhonePad("***2225#"),                  // Edge Case 3: CJ (Do not remove next letters when extra backspace pressed before)
+                Phone.OldPhonePad("**#"),                       // Edge Case 4: (Empty output when only backspace but no other key pressed)
+                Phone.OldPhonePad("#")                          // Edge Case 5: (Empty output when nothing pressed.)
             };
             
             
