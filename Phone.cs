ff2ac35b6fd6680a15c0c32230debd792e16699d
Ikramul Haque Oli <ikramul.haque@selisegroup.com>
2024-05-22 17:44:03 +0600

    Write solution to OldPhonePad

---
+++ Phone.cs
@@ -0,0 +1,58 @@
+namespace Assessment_Iron_Software
+{
+    public static class Phone
+    {
+        private static Dictionary<char, string> oldPhoneButtonMap = 
+            new Dictionary<char, string>()
+            {
+                {'0', " "},
+                {'1', "&'("},
+                {'2', "abc"},
+                {'3', "def"},
+                {'4', "ghi"},
+                {'5', "jkl"},
+                {'6', "mno"},
+                {'7', "pqrs"},
+                {'8', "tuv"},
+                {'9', "wxyz"}
+            };
+
+        public static string OldPhonePad(string input)
+        {
+            string result = string.Empty;
+
+            int sameCharStrict = 1;
+            char previousChar = '!';
+            
+            foreach (var currentChar in input)
+            {
+                if (currentChar == previousChar)
+                {
+                    sameCharStrict++;
+                }
+                else
+                {
+                    if (previousChar >= '0' && previousChar <= '9')
+                    {
+                        var buttonString = oldPhoneButtonMap[previousChar];
+                        var resultLetterIndex = (sameCharStrict-1) % buttonString.Length;
+                        result += buttonString[resultLetterIndex];
+                    }
+                    else if (previousChar == '*')
+                    {
+                        var removeCount = Math.Min(result.Length, sameCharStrict);
+                        result = result.Substring(0, result.Length - removeCount);
+                    }
+
+                    sameCharStrict = 1;
+                }
+
+                if (currentChar == '#') break;
+                
+                previousChar = currentChar;
+            }
+
+            return result.ToUpper();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assessment_Iron_Software
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MainClass
+    {
+        public static void Main(params string[] args)
+        {
+            var results = new List<string> {
+                Phone.OldPhonePad("33#"),
+                Phone.OldPhonePad("227*#"),
+                Phone.OldPhonePad("4433555 555666#"),
+                Phone.OldPhonePad("8 88777444666*664#"),
+                Phone.OldPhonePad("22222#"),
+                Phone.OldPhonePad("#")
+            };
+            
+            
+            results.ForEach(result => Console.WriteLine("Output: " + result));
+        }
+    }
+}
